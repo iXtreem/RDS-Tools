f0402efb5c4d9f199875e22f1119ace4670d6595
iXtreem <107632266+iXtreem@users.noreply.github.com>
2023-12-21 05:27:27 +0700

    Add files via upload

--- my_lib.lua
+++ my_lib.lua
@@ -72,19 +72,28 @@ function sampGetPlayerIdByNickname(nick) -- узнать ID по нику
 	for i = 0, 301 do if sampIsPlayerConnected(i) and sampGetPlayerNickname(i) == nick then return i end end
 end
 
-function string.rlower(s) -- перевод русских букв в прописные
-	local s = s:lower()
-    local strlen = s:len()
-    if strlen == 0 then return s end
-    local output = ''
-	local russian_characters = {[168] = 'Ё', [184] = 'ё', [192] = 'А', [193] = 'Б', [194] = 'В', [195] = 'Г', [196] = 'Д', [197] = 'Е', [198] = 'Ж', [199] = 'З', [200] = 'И', [201] = 'Й', [202] = 'К', [203] = 'Л', [204] = 'М', [205] = 'Н', [206] = 'О', [207] = 'П', [208] = 'Р', [209] = 'С', [210] = 'Т', [211] = 'У', [212] = 'Ф', [213] = 'Х', [214] = 'Ц', [215] = 'Ч', [216] = 'Ш', [217] = 'Щ', [218] = 'Ъ', [219] = 'Ы', [220] = 'Ь', [221] = 'Э', [222] = 'Ю', [223] = 'Я', [224] = 'а', [225] = 'б', [226] = 'в', [227] = 'г', [228] = 'д', [229] = 'е', [230] = 'ж', [231] = 'з', [232] = 'и', [233] = 'й', [234] = 'к', [235] = 'л', [236] = 'м', [237] = 'н', [238] = 'о', [239] = 'п', [240] = 'р', [241] = 'с', [242] = 'т', [243] = 'у', [244] = 'ф', [245] = 'х', [246] = 'ц', [247] = 'ч', [248] = 'ш', [249] = 'щ', [250] = 'ъ', [251] = 'ы', [252] = 'ь', [253] = 'э', [254] = 'ю', [255] = 'я'}
-    for i = 1, strlen do
-        local ch = s:byte(i)
-        if ch >= 192 and ch <= 223 then output = output .. russian_characters[ch + 32]
-        elseif ch == 168 then output = output .. russian_characters[184] -- буква ё
-		else output = output .. string.char(ch) end
-    end
-    return output
+
+local russian_characters = {
+	['Ё'] = 'ё', ['А'] = 'а', ['Б'] = 'б', ['В'] = 'в', ['Г'] = 'г', ['Д'] = 'д', ['Е'] = 'е', ['Ж'] = 'ж',
+	['З'] = 'з', ['И'] = 'и', ['П'] = 'п', ['Н'] = 'н', ['О'] = 'о', ['Ф'] = 'ф', ['Х'] = 'х', ['Ц'] = 'ц', ['Р'] = 'р',
+	['Ы'] = 'ы', ['Ь'] = 'ь', ['Э'] = 'э', ['Й'] = 'й', ['Р'] = 'р', ['Ч'] = 'ч', ['Ю'] = 'ю', ['К'] = 'к', ['С'] = 'с',
+	['Ш'] = 'ш', ['Я'] = 'я', ['Л'] = 'л', ['Т'] = 'т', ['Щ'] = 'щ', ['М'] = 'м', ['У'] = 'у', ['Ъ'] = 'ъ', ['['] = '%['
+}
+
+
+function string.rlower(text)
+	if #text == 0 then return text end -- если длина строки 0 - не нагружаем пк
+	local text = text:lower()	   -- переводим буквы на английском в маленькие
+	local output = ''			   -- строка вывода
+	for i = 1, string.len(text) do -- перебор элементов
+		local key = text:sub(i, i)	-- буквы поочередно абоба - а б о б а
+		if russian_characters[key] then	-- если буква есть в массиве
+			output = output ..( text:sub(i, i-1) .. russian_characters[key]) -- заменить эту букву на новую
+ 		else 
+			output = output .. key -- вернуть прежнюю
+		end
+	end
+	return output -- вернуть результат
 end
 
 function imgui.NewInputText(lable, val, width, hint, hintpos) -- Поле ввода с подсказкой
@@ -260,34 +269,29 @@ end
 
 local transliterationTable = {
     ['й'] = 'q', ['ц'] = 'w', ['у'] = 'e', ['к'] = 'r', ['е'] = 't', ['н'] = 'y', ['г'] = 'u', ['ш'] = 'i', ['щ'] = 'o',
-    ['з'] = 'p', ['х'] = '[', ['ъ'] = ']', ['ф'] = 'a', ['ы'] = 's', ['в'] = 'd', ['а'] = 'f', ['п'] = 'g', ['р'] = 'h',
-    ['о'] = 'j', ['л'] = 'k', ['д'] = 'l', ['ж'] = ';', ['э'] = "'", ['я'] = 'z', ['ч'] = 'x', ['с'] = 'c', ['м'] = 'v',
-    ['и'] = 'b', ['т'] = 'n', ['ь'] = 'm', ['б'] = ',', ['ю'] = '.', ['ё'] = '`',
+    ['з'] = 'p', ['х'] = '%[', ['ъ'] = '%]', ['ф'] = 'a', ['ы'] = 's', ['в'] = 'd', ['а'] = 'f', ['п'] = 'g', ['р'] = 'h',
+    ['о'] = 'j', ['л'] = 'k', ['д'] = 'l', ['ж'] = '%;', ['э'] = "%'", ['я'] = 'z', ['ч'] = 'x', ['с'] = 'c', ['м'] = 'v',
+    ['и'] = 'b', ['т'] = 'n', ['ь'] = 'm', ['б'] = '%,', ['ю'] = '%.', ['ё'] = '%`',
     
     ['q'] = 'й', ['w'] = 'ц', ['e'] = 'у', ['r'] = 'к', ['t'] = 'е', ['y'] = 'н', ['u'] = 'г', ['i'] = 'ш', ['o'] = 'щ',
     ['p'] = 'з', ['['] = 'х', [']'] = 'ъ', ['a'] = 'ф', ['s'] = 'ы', ['d'] = 'в', ['f'] = 'а', ['g'] = 'п', ['h'] = 'р',
-    ['j'] = 'о', ['k'] = 'л', ['l'] = 'д', [';'] = 'ж', ["'"] = 'э', ['z'] = 'я', ['x'] = 'ч', ['c'] = 'с', ['v'] = 'м',
-    ['b'] = 'и', ['n'] = 'т', ['m'] = 'ь', [','] = 'б', ['.'] = '/', ['`'] = 'ё', ['/'] = '.',
+    ['j'] = 'о', ['k'] = 'л', ['l'] = 'д', ['%;'] = 'ж', ["%'"] = 'э', ['z'] = 'я', ['x'] = 'ч', ['c'] = 'с', ['v'] = 'м',
+    ['b'] = 'и', ['n'] = 'т', ['m'] = 'ь', ['%,'] = 'б', ['%.'] = '/', ['%`'] = 'ё', ['/'] = '%.',
 }
 
-function translateText(input)
-    local output = ''
-    for i = 1, string.len(input) do
-		local translatedChar = nil
-        local char = string.sub(input, i, i)
-		for k,v in pairs(transliterationTable) do
-        	if k == char then
-				translatedChar = v
-				break
-			elseif v == chat then
-				translatedChar = k
-				break
-			end
+function translateText(text)
+	if #text == 0 then return text end -- если длина строки 0 - не нагружаем пк
+	local text = string.rlower(text)
+	local output = ''			   -- строка вывода
+	for i = 1, string.len(text) do -- перебор элементов
+		local key = text:sub(i, i)	-- буквы поочередно абоба - а б о б а
+		if transliterationTable[key] then	-- если буква есть в массиве
+			output = output ..( text:sub(i, i-1) .. transliterationTable[key]) -- заменить эту букву на новую
+ 		else 
+			output = output .. key -- вернуть прежнюю
 		end
-        if translatedChar then output = output .. translatedChar
-		else output = output .. char end
-    end
-    return output
+	end
+	return output -- вернуть результат
 end
 function encrypt(text, shift)
 	local encrypted = ""
